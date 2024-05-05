a9199c00b52d4bea23eded742abd2b8e53f6fa0c
Paul Wilkinson <cremoni@gmail.com>
2024-05-05 11:26:43 +0200

    Lots of fixes

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,29 @@
 import * as core from '@actions/core';
 import * as github from '@actions/github';
 
-try {
-    // `who-to-greet` input defined in action metadata file
-    const nameToGreet = core.getInput('who-to-greet');
-    console.log(`Hello ${nameToGreet}!`);
-    const time = (new Date()).toTimeString();
-    core.setOutput("time", time);
-    // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`);
-} catch (error) {
-    core.setFailed(error.message);
+function greet() {
+    try {
+        // `who-to-greet` input defined in action metadata file
+        const nameToGreet = core.getInput('who-to-greet');
+        console.log(`Hello ${nameToGreet}!`);
+        const time = (new Date()).toTimeString();
+        core.setOutput("time", time);
+        // Get the JSON webhook payload for the event that triggered the workflow
+        const payload = JSON.stringify(github.context.payload, undefined, 2)
+        console.log(`The event payload: ${payload}`);
+    } catch (error) {
+        if (error instanceof Error) {
+            core.setFailed(error.message); // Properly log the error message and fail the action
+        } else {
+            console.log('Failed to greet');
+        }
+    }
 }
+
+function main() {
+    greet();
+}
+
+main();
+
+export default greet;
